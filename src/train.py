39a54d8732d61dba6affb1924cfa0ba7b9a98eec
Subhash <subhbishnoi@users.noreply.github.com>
2026-03-10 10:58:38 +0530

    Lab 1: ML project setup

--- src/train.py
+++ src/train.py
@@ -1,1 +1,11 @@
-print('hello mlops')
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+import joblib, os
+
+X, y = load_iris(return_X_y=True)
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X, y)
+
+os.makedirs("models", exist_ok=True)
+joblib.dump(model, "models/model.pkl")
+print("Model trained and saved!")
